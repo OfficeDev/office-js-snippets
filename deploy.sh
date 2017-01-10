dd4f27d820253053ad99dd69f908e223513c5239
Bhargav Krishna <bhkrish@microsoft.com>
2017-01-09 16:58:04 -0800

    removing clone process

--- deploy.sh
+++ deploy.sh
@@ -14,24 +14,13 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-# Clone the existing repo into `out`and cd into it
-git clone $REPO out
-cd out
-
 # Run `npm install` and our `build` script
-npm install
 npm run build
 git status
 
 # Now let's go have some fun with the cloned repo
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
-
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.

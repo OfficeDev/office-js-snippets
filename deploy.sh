bacdd381916b8be7573273db53ba79302bf9a0ce
Bhargav Krishna <bhkrish@microsoft.com>
2017-01-09 17:11:29 -0800

    adding github check

--- deploy.sh
+++ deploy.sh
@@ -18,8 +18,6 @@ SHA=`git rev-parse --verify HEAD`
 git clone $REPO out
 cd out
 
-echo $REPO
-
 # Run `npm install` and our `build` script
 npm install
 npm run build
@@ -28,6 +26,12 @@ git status
 # Now let's go have some fun with the cloned repo
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
+
+# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+if [ -z `git diff --exit-code` ]; then
+    echo "No changes to the output on this push; exiting."
+    exit 0
+fi
 
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
@@ -45,4 +49,4 @@ eval `ssh-agent -s`
 ssh-add deploy_key
 
 # Now that we're all set up, we can push.
-git push $SSH_REPO $BRANCH
+git push $SSH_REPO $TARGET_BRANCH

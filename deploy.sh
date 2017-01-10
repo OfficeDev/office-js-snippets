ab43ab30ff487f95ad1de9d9d7092963da49ee20
Bhargav Krishna <bhkrish@microsoft.com>
2017-01-09 17:20:00 -0800

    removing playlists on master branch and updating  deploy script

--- deploy.sh
+++ deploy.sh
@@ -29,12 +29,6 @@ git status
 git config user.name "Travis CI"
 git config user.email "$COMMIT_AUTHOR_EMAIL"
 
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
-
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add .
@@ -51,4 +45,4 @@ eval `ssh-agent -s`
 ssh-add deploy_key
 
 # Now that we're all set up, we can push.
-git push -u origin/deployment
+git push -u origin deployment
